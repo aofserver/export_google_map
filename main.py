0610934e73695f424bcabb3c17cfd5a9a5d7a05d
aofserver <mr.sarawutnacwijit@gmail.com>
2024-11-26 13:42:46 +0700

    update

--- main.py
+++ main.py
@@ -18,38 +18,53 @@ with open('provinces-th/tambons.json', 'r') as file:
     tambons = json.load(file)
     tambons = sorted(tambons, key=lambda x: x['id'])
 
-keyword = [
-    "อู่เปลี่ยนถ่ายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
-    "ร้านขายเครื่องมือและอุปกรณ์การเกษตรใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
-    "ร้านขายอะไหล่รถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
-    "ศูนย์ซ่อมรถยนต์และรถบรรทุกใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
-    "อู่เปลี่ยนถ่ายน้ำมันเครื่องมอเตอร์ไซค์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+# keyword = [
+#     "อู่เปลี่ยนถ่ายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "ร้านขายเครื่องมือและอุปกรณ์การเกษตรใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "ร้านขายอะไหล่รถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "ศูนย์ซ่อมรถยนต์และรถบรรทุกใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "อู่เปลี่ยนถ่ายน้ำมันเครื่องมอเตอร์ไซค์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
     
-    "ร้านขายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
-    "ร้านขายน้ำมันเครื่องมอเตอร์ไซค์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "ร้านขายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "ร้านขายน้ำมันเครื่องมอเตอร์ไซค์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "ร้านซ่อมจักรยานยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+#     "อู่ซ่อมรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}"
+# ]
+
+keyword = [
+    # "อู่เปลี่ยนถ่ายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    # "ร้านขายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    # "ร้านขายน้ำมันเครื่องมอเตอร์ไซค์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    # "อู่เปลี่ยนถ่ายน้ำมันเครื่องรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    # "อู่เปลี่ยนถ่ายน้ำมันเครื่องมอเตอร์ไซค์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
     "ร้านซ่อมจักรยานยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
-    "อู่ซ่อมรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}"
+    "ร้านขายเครื่องมือและอุปกรณ์การเกษตรใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    "ศูนย์ซ่อมรถยนต์และรถบรรทุกใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    "อู่ซ่อมรถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}",
+    "ร้านขายอะไหล่รถยนต์ใน จังหวัด{province} อำเภอ{amphure} ตำบล{tambon}"
 ]
 
 output_all = []
 for id_k, k in enumerate(keyword):
+  if os.getenv('keyword_id'):
+    if id_k != int(os.getenv('keyword_id',"0")):
+      continue
   len_filtered_provinces = len(provinces)
   for id_p, p in enumerate(provinces):
-    if os.getenv('province_id'):
-      if p["id"] != int(os.getenv('province_id')):
-        continue
+    if p["id"] != int(os.getenv('province_id',"0")):
+      continue
     print("...",f"{id_p+1}/{len_filtered_provinces}", p["name_th"], p["id"], id_k)
     filtered_amphures = [amphure for amphure in amphures if amphure["province_id"] == p["id"]]
     len_filtered_amphures = len(filtered_amphures)
     for id_a, a in enumerate(filtered_amphures):
-      if id_a < int(os.getenv('amphure_id')):
+      if id_a < int(os.getenv('amphure_id',"0")):
         continue
       print(".....",f"{id_a+1}/{len_filtered_amphures}", a["name_th"], a["id"])
       filtered_tambons = [tambon for tambon in tambons if tambon["amphure_id"] == a["id"]]
       len_filtered_tambons = len(filtered_tambons)
       for id_t, t in enumerate(filtered_tambons):
-        if id_t < int(os.getenv('tambon_id')):
-          continue
+        # if id_t < int(os.getenv('tambon_id',"0")):
+        #   continue
         print("........",f"{id_t+1}/{len_filtered_tambons}", t["name_th"], t["id"])
         textQuery = k.format(province=p["name_th"], amphure=a["name_th"], tambon=t["name_th"])
         result = google_map.QueryGoogleMap(textQuery)
--- summary_file.py
+++ summary_file.py
@@ -4,27 +4,54 @@ import csv
 from pathlib import Path
 
 
-def Save():
+# def Save():
+#     # find data not duplicate 
+#     result = []
+#     path = 'output/data.json'
+#     if os.path.isfile(path):
+#         with open(path, 'r') as file:
+#             result = json.load(file)
+
+#     list_file = os.listdir("output")
+#     for filename in list_file:
+#         if Path(filename).suffix != ".json" and filename == "data.json":
+#             continue
+#         with open(f'output/{filename}', 'r') as file:
+#             data_all = json.load(file)
+#         for data in list(data_all):
+#             check = [r for r in result if r["id"] == data["id"]]
+#             if not check:
+#                 result.append(data)
+#         if Path(filename).suffix == ".json" and filename != "data.json":
+#             os.remove(f'output/{filename}')
+
+#     # write json file
+#     with open(f"output/data.json", "w", encoding='utf-8') as f:
+#         json.dump(result, f, ensure_ascii=False, indent=4)
+
+
+
+def Save(folder="output"):
     # find data not duplicate 
     result = []
-    path = 'output/data.json'
+    path = f'{folder}/data.json'
     if os.path.isfile(path):
         with open(path, 'r') as file:
             result = json.load(file)
 
-    list_file = os.listdir("output")
+    list_file = os.listdir(folder)
     for filename in list_file:
         if Path(filename).suffix != ".json" and filename == "data.json":
             continue
-        with open(f'output/{filename}', 'r') as file:
+        with open(f'{folder}/{filename}', 'r') as file:
             data_all = json.load(file)
         for data in list(data_all):
             check = [r for r in result if r["id"] == data["id"]]
             if not check:
                 result.append(data)
         if Path(filename).suffix == ".json" and filename != "data.json":
-            os.remove(f'output/{filename}')
+            os.remove(f'{folder}/{filename}')
 
     # write json file
-    with open(f"output/data.json", "w", encoding='utf-8') as f:
+    with open(f"{folder}/data.json", "w", encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=4)
